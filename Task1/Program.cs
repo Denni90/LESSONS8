d2276f4c28fa0c57e3f8894f76af4cadb824c912
denis.tsapenko <denis.tsapenko90@yandex.ru>
2022-09-05 23:34:52 +0300

    Решение задач

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,5 +8,64 @@
 2 3 5 9
 2 4 4 8*/
 
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int range = Numbers("Введите диапазон: от 1 до ");
+int[,] array = new int[m, n];
 
+void ArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+        if (array[i, k] < array[i, k + 1])
+        {
+            int temp = array[i, k + 1];
+            array[i, k + 1] = array[i, k];
+            array[i, k] = temp;
+        }
+        }
+    }
+    }
+}
 
+int Numbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(range);
+    }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+        Console.WriteLine();
+    }
+}
+
+CreateArray(array);
+WriteArray(array);
+Console.WriteLine($"\nОтсортированный массив: ");
+ArrayLines(array);
+WriteArray(array);
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -15,3 +15,75 @@
 56 8 4 24
 10 6 24 49*/
 
+Console.WriteLine("Введите m 1-й матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n 1-й матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите l 2-й матрицы: ");
+int l = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите p 2-й матрицы: ");
+int p = Convert.ToInt32(Console.ReadLine());
+
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[l, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int k = 0; k < firstMartrix.GetLength(1); k++)
+        {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+        }
+        resultMatrix[i,j] = sum;
+    }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    for (int j = 0; j < array.GetLength(1); j++)
+        {
+        Console.Write(array[i,j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
